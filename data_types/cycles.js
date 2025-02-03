372e310eb3ef788b33a49f60e7422178ba38c471
Александр <bmwe34alexandr@icloud.com>
2025-02-03 13:44:47 +0300

    Задачи с циклами

---
+++ data_types/cycles.js
@@ -0,0 +1,36 @@
+// for (let array = 0; array <= 100; array++) {
+//     if (array % 2 === 0) {
+//         console.log(array);
+//     }
+// }
+
+
+// let arr = [1, 2, 3, 4, 5];
+// let accum = 0;
+// for (let i = 0; i < arr.length; i++) {
+//     (accum = accum + arr[i]);
+// }
+// console.log(accum);
+
+// let arr = [1, 2, 3, 4, 5];
+// const result = arr.reduce(function (acc, item) {
+//     acc += item;
+//     return acc
+// })
+// console.log(result);
+
+// let arr = [2, 2, 2, 2];
+// const result = arr.reduce((acc, item) => acc + item);
+// console.log(result);
+
+// let obj = {
+//     Коля: 200,
+//     Вася: 300,
+//     Петя: 400
+// };
+
+// for (key in obj) {
+
+//     console.log(key + "-зарплата" + obj[key] + "долларов");
+// }
+
