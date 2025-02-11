d7677dae8a59864bc598e5b5bbe04c3c5b26457f
Александр <bmwe34alexandr@icloud.com>
2025-02-11 22:04:40 +0300

    Решение задач в теме Object

---
+++ object.js
@@ -0,0 +1,24 @@
+// let user = {}
+// user.name = "John",
+//     user.surname = "Smith"
+// user.name = "Pete"
+// delete user.name
+
+// function isEmpty(obj) {
+//     for (key in obj) {
+//         return false
+//     }
+//     return true
+// }
+// console.log(isEmpty("pop"));
+
+// let salaries = {
+//     John: 100,
+//     Ann: 160,
+//     Pete: 130
+// }
+// sum = 0;
+// for (key in salaries) {
+//     sum += salaries[key]
+// }
+// console.log(sum);
