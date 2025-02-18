10e495da8ce96976ecc485b5114762f1c0ade787
Александр <bmwe34alexandr@icloud.com>
2025-02-18 10:59:30 +0300

    Конструктор new

--- object.js
+++ object.js
@@ -22,3 +22,79 @@
 //     sum += salaries[key]
 // }
 // console.log(sum);
+
+// let calculator = {
+//     read() {
+//         this.a = +prompt("Введите число");
+//         this.b = +prompt("Введите число");
+//     },
+//     sum() {
+//         return this.a + this.b
+//     },
+//     mul() {
+//         return this.a * this.b
+//     }
+// }
+// calculator.read();
+// alert( calculator.sum() );
+// alert( calculator.mul() );
+
+// let ladder = {
+//     step: 0,
+//     up() {
+//         return this.step++;
+//     },
+//     down() {
+//         return this.step--;
+//     },
+//     showStep: function () { // показывает текущую ступеньку
+//         console.log(this.step);
+//     }
+// };
+
+// ladder.up();
+// ladder.up();
+// ladder.down();
+// ladder.showStep();
+// ladder.down();
+// ladder.showStep();
+
+
+// function Accumulator(startingValue) {
+//     this.value = startingValue;
+
+//     this.read = function () {
+//      this.num = +prompt("Введите число");
+//       this.value += this.num
+
+
+//     };
+// };
+
+// let accumulator = new Accumulator(1);
+
+// accumulator.read();
+// accumulator.read();
+
+// alert(accumulator.value);
+
+// function Calculator() {
+
+
+//     this.read = function () {
+//         this.a = +prompt("Введите число");
+//         this.b = +prompt("Введите число");
+//     },
+//         this.sum = function () {
+//             return this.a + this.b
+//         },
+//         this.mul = function () {
+//             return this.a * this.b
+//         }
+// }
+
+// let calculator = new Calculator();
+// calculator.read();
+
+// alert("Sum=" + calculator.sum());
+// alert("Mul=" + calculator.mul());
